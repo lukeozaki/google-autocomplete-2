dd402811ef0cae4fa3f7bad7d5e8bc27993598f9
lukeozaki <103615984+lukeozaki@users.noreply.github.com>
2024-03-07 01:18:00 +0900

    directory sort by slug

--- src/components/directory/DirectoryList.tsx
+++ src/components/directory/DirectoryList.tsx
@@ -32,14 +32,23 @@ interface DirectoryListLayoutProps {
   relativePrefixToRoot: string;
 }
 
+const sortChildren = (a, b) => {
+  return a.slug < b.slug
+    ? -1
+    : a.slug > b.slug
+    ? 1
+    : 0;
+};
+
 // This template renders the data into HTML
 const DirectoryListLayout = (props: DirectoryListLayoutProps) => {
   const { showNumLocs, directoryChildren, relativePrefixToRoot } = props;
+  const sortedChildren = directoryChildren?.sort(sortChildren);
   return (
     <ErrorBoundaryWithAnalytics name="directory">
       <div className="container my-8">
         <ul className="lg:columns-4 md:columns-3 sm:columns-2 columns-1 -m-3">
-          {directoryChildren.map((child, idx) => (
+          {sortedChildren.map((child, idx) => (
             <li className="p-3" key={idx}>
               <Link
                 className="inline-block after:content-[attr(data-count)] after:ml-2"
